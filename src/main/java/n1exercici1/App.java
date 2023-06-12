eef3ee1396bbe978b1cbb261b5af4f383918d870
Joren <97386268+Aebal34@users.noreply.github.com>
2023-06-12 13:00:17 +0200

    implementation of Undo class

--- src/main/java/n1exercici1/App.java
+++ src/main/java/n1exercici1/App.java
@@ -1,5 +1,22 @@
 package n1exercici1;
 
 public class App {
-
+	public static void main(String[] args) {
+		
+		boolean flag = true;
+		while(flag) {
+			
+			Undo.getInstance().createState(Input.readString("Write something:"));
+			if(Input.readYesOrNo("Do you want to undo?")) {
+				Undo.getInstance().undoState();
+			}
+			System.out.println("Your history is as follows: ");
+			for (String content : Undo.getInstance().getHistory()) {
+				System.out.println(content);
+			}
+			if(!Input.readYesOrNo("Do you want to keep writing? y/n")) {
+				flag = false;
+			}
+		}
+	}
 }
--- src/main/java/n1exercici1/Undo.java
+++ src/main/java/n1exercici1/Undo.java
@@ -1,21 +1,39 @@
 package n1exercici1;
 
+import java.util.*;
+
 public class Undo {
 	
 	//Singleton's static instance
 	private static Undo instance;
+	
+	private List<String> history = new ArrayList<String>();
 	
 	//Singleton's private constructor
 	private Undo() {
 		
 	}
 	
+	public List<String> getHistory(){
+		return history;
+	}
 	
-	//Singleton's get intstance, so the class is always the same and we can't have more than 1 instance of it
+	//Singleton's get instance, so the class is always the same and we can't have more than 1 instance of it
 	public static Undo getInstance() {
 		if(instance == null) {
 			instance = new Undo();
 		}
 		return instance;
+	}
+	
+	public void createState(String content) {
+		history.add(content);
+	}
+	
+	public void undoState() {
+		
+		if(history.size()-1 >= 0) {
+			history.remove(history.size()-1);
+		}
 	}
 }
