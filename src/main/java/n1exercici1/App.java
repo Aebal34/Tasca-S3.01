9da3a3acae491c3a3126163d9bd0e51f746bcdc1
Joren <97386268+Aebal34@users.noreply.github.com>
2023-06-13 11:54:39 +0200

    minor improvements

--- src/main/java/n1exercici1/App.java
+++ src/main/java/n1exercici1/App.java
@@ -3,15 +3,17 @@ package n1exercici1;
 public class App {
 	public static void main(String[] args) {
 		
+		//We store the instance of Undo class to access to it quickly
+		Undo undo = Undo.getInstance();
 		boolean flag = true;
 		while(flag) {
 			
-			Undo.getInstance().createState(Input.readString("Write something:"));
+			undo.createState(Input.readString("Write something:"));
 			if(Input.readYesOrNo("Do you want to undo?")) {
-				Undo.getInstance().undoState();
+				undo.undoState();
 			}
 			System.out.println("Your history is as follows: ");
-			for (String content : Undo.getInstance().getHistory()) {
+			for (String content : undo.getHistory()) {
 				System.out.println(content);
 			}
 			if(!Input.readYesOrNo("Do you want to keep writing? y/n")) {
